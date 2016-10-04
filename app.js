6d927fbb91434aaadf78c95e45d278d5aca9b548
dallin-r-parker <dallin.r.parker@gmail.com>
2016-10-03 21:27:10 -0600

    finished

---
+++ app.js
@@ -0,0 +1,13 @@
+angular.module('quizApp', ['ui.router'])
+  //  .config(function($stateProvider, $urlRouterProvider) {
+  //
+  //   $urlRouterProvider.otherwise('/')
+   //
+  //       $stateProvider
+  //         .state('☄',{
+  //           url: '/',
+  //           templateUrl: 'placeholder',
+  //           controller: 'placeholder'
+  //         })
+  //
+  })
